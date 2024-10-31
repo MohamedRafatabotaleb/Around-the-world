71075ad02e40767ed75b35a2f5f0d97871fb9921
Mohamed Rafat <121605610+MohamedRafatabotaleb@users.noreply.github.com>
2024-10-31 10:23:55 +0300

    herader del

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
         </Route>
       </Routes>
       <div className="min-h-screen w-screen bg-gray-100 font-inter dark:bg-gray-900 dark:text-gray-100">
-        <Header />
         <div className="container mx-auto px-5 md:px-0"></div>
       </div>
     </BrowserRouter>
