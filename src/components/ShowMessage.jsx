333fc21a940acabdea6386a4d037e518813ab984
Mohamed Rafat <121605610+MohamedRafatabotaleb@users.noreply.github.com>
2024-10-31 09:54:42 +0300

    Main

---
+++ src/components/ShowMessage.jsx
@@ -0,0 +1,9 @@
+const ShowMessage = ({ message }) => {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">{message}</h1>
+    </div>
+  );
+};
+
+export default ShowMessage;
